b9bc44a1eb8d61f3d774f48315d9b3f427fa893b
OHOKs <85952515+OHOKs@users.noreply.github.com>
2024-10-21 12:45:11 +0200

    Add files via upload

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -2,8 +2,27 @@ const connect = require("../config/db");
 
 const getAllGames = async (req, res) => {
     try{
-        const users = await new Promise((resolve, reject) => {
-            connect.query("SELECT * FROM `game`", (err, result) => {
+        const users = await new Promise(async (resolve, reject) => {
+            connect.query(`
+    SELECT 
+       game.id AS game_id,
+       game.name AS game_name,
+       game.playerCount,
+       game.playerPerTeam,
+       game.requiredForPrize,
+       game.createdAt AS game_createdAt,
+       game.updatedAt AS game_updatedAt,
+       variant.name AS variant_name,
+       matchtype.type AS match_type
+    FROM 
+        game
+    JOIN 
+        gamevariants ON game.id = gamevariants.gameId
+    JOIN 
+        variant ON gamevariants.variantId = variant.id
+    JOIN 
+        matchtype ON gamevariants.typeId = matchtype.id;
+`, (err, result) => {
                 if (err) reject(err); 
                 else resolve(result);
             });
